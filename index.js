5aab2f5a2f6caecccdc6de563c03477a48e3f1f0
S UNNIKRISHNAN <97491531+unnikrishnansk@users.noreply.github.com>
2022-10-12 16:33:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+
+const app = express();
+
+const Validation = {
+    body: ({
+      ID: number()
+        .required(),
+      Name: string()
+        .required(),
+        Rating: number()
+        .required(),
+        Description: string()
+        .required(),
+        Genre: string()
+        .required(),
+        Cast: string()
+        .required(),
+    }),
+  }
+  
+  app.use(bodyParser.json())
+  
+  app.post('/movies', validate(Validation, {}, {}), (req, res) => {
+    res.json(200)
+  })
+  
+  app.use((err, req, res, next) => {
+    if (err) 
+    {
+      return res.status(err.statusCode).json(err)
+    }
+  
+    return res.status(500).json(err)
+  })
+
+app.listen(8000,() => {
+    console.log("listening to port 8000");
+})
